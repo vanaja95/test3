f26f712643582d7f64afaf4c4b51dc6eea8c032a
vanaja95 <vanajareddy277@gamail>
2023-09-19 22:31:56 -0700

    program

---
+++ D.java
@@ -0,0 +1,11 @@
+class D 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hii maaa ");
+		System.out.println("hii maaa ");
+		System.out.println("hii maaa ");
+		System.out.println("hii maaa ");
+		System.out.println("hii maaa ");
+	}
+}
